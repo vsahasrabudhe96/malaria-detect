1c50553e1597a7c372c8e904311a4fb8ecfb96bd
Varun Sahasrabudhe <59940037+vsahasrabudhe96@users.noreply.github.com>
2020-07-21 16:29:10 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 import cv2
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import PIL
 
 model = load_model('./model/model.h5')
